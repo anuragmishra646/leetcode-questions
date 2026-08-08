5cf673c53a636a0dd1bb81de8388acb879c57d21
anuragmishra646 <am7493923@gmail.com>
2026-08-08 22:22:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.37%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for(int i = 0 ; i<word.size();i++){
+            if(isupper(word[i])){
+                count++;
+            }
+        }
+        if(count == 0){
+            return true;
+        }
+        if(count ==word.size()){
+            return true;
+        }
+        if(count == 1&& isupper(word[0]) ){
+            return true;
+        }
+        return false;
+    }
+};
